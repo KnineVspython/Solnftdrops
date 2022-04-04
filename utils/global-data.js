1f5e500bf70a8a4ae36066b339b9d63abdc93e19
kamal shaw <44661274+KnineVspython@users.noreply.github.com>
2022-04-03 20:07:34 -0400

    Update

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Sol Nft Drops';
+    : 'Degen Dragon DAO| MADE WITH ❤️ IN DEGEN';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Featured Drops';
