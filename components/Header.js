db34ba38d1da52ec2481e102acb852107d756827
kamal shaw <44661274+KnineVspython@users.noreply.github.com>
2022-04-03 20:06:01 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <img src="https://media.discordapp.net/attachments/898657008553701406/960317925754040340/solnftdropslogo.png"></img>
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
+        <Link href="https://discord.gg/mZEgBr7r">
           <a>{name}</a>
         </Link>
       </p>
